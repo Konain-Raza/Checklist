d6da8a1add22fa48adf5a0ff9fa30b60d09bb27d
Konain Raza <konainraza105.com>
2024-11-21 18:39:08 +0500

    completed requirements

--- src/frontend/Dashboard/index.jsx
+++ src/frontend/Dashboard/index.jsx
@@ -8,10 +8,17 @@ import ForgeReconciler, {
   Label,
   Text,
   List,
+  Icon,
   SectionMessage,
-  SectionMessageAction,
+  Spinner,
   ListItem,
-  Popup,
+  Modal,
+  ModalHeader,
+  ModalTitle,
+  ModalBody,
+  ModalFooter,
+  ModalTransition,
+  xcss,
 } from "@forge/react";
 import { invoke } from "@forge/bridge";
 
@@ -22,7 +29,8 @@ const App = () => {
   const [newListName, setNewListName] = useState("");
   const [newListItems, setNewListItems] = useState("");
   const [message, setMessage] = useState("");
-  const [openPopupIndex, setOpenPopupIndex] = useState(null); // Track the currently open popup index
+  const [loading, setLoading] = useState(true);
+  const [activeModal, setActiveModal] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -33,7 +41,6 @@ const App = () => {
           const allTemplates = await invoke("getTemplates", {
             email: userData.emailAddress,
           });
-          console.log(allTemplates);
           if (Array.isArray(allTemplates)) {
             setTemplates(allTemplates);
           } else {
@@ -43,15 +50,16 @@ const App = () => {
           throw new Error("User email address not found.");
         }
       } catch (error) {
-        console.error(error);
         setMessage("Error loading templates.");
+      } finally {
+        setLoading(false);
       }
     };
     fetchData();
   }, []);
 
   const handleSave = async () => {
-    if (!newListName || !newList.items.length) {
+    if (!newListName.trim() || newList.items.length === 0) {
       setMessage("Please enter a list name and items.");
       return;
     }
@@ -59,34 +67,31 @@ const App = () => {
     const updatedList = { name: newListName.trim(), items: [...newList.items] };
     const allTemplates = [...templates, updatedList];
     setTemplates(allTemplates);
-    console.log(allTemplates);
+    handleClear();
 
     try {
       const response = await invoke("setTemplates", {
         email: email.trim(),
         templates: allTemplates,
       });
-      if (response?.success) {
-        setMessage("List saved successfully.");
-      } else {
-        setMessage("Failed to save the list.");
-      }
-    } catch (error) {
-      console.error(error);
+      setMessage(
+        response?.success
+          ? "List saved successfully."
+          : "Failed to save the list."
+      );
+    } catch {
       setMessage("Error saving the list.");
     }
-    handleClear();
   };
 
   const handleClear = () => {
     setNewListName("");
     setNewListItems("");
     setNewList({ name: "", items: [] });
-    setMessage("List cleared.");
   };
 
   const addListItems = () => {
-    if (!newListItems || newListItems.trim().length === 0) {
+    if (!newListItems.trim()) {
       setMessage("Please enter an item.");
       return;
     }
@@ -101,96 +106,160 @@ const App = () => {
     setNewListItems("");
   };
 
-  const handleViewTasks = (templateName) => {
-    console.log(`Viewing tasks for ${templateName}`);
-    // Add your task viewing logic here
-  };
+  const createNewListBlock = xcss({
+    backgroundColor: "color.background.neutral.subtle.hovered",
+    padding: "space.200",
+    width: "600px",
+  });
 
-  const togglePopup = (index) => {
-    setOpenPopupIndex(openPopupIndex === index ? null : index); // Toggle the popup
+  const ListBlockStyles = xcss({
+    backgroundColor: "color.background.neutral.subtle.hovered",
+    padding: "space.200",
+  });
+
+  const handleDeleteList = async (index) => {
+    const updatedTemplates = templates.filter((_, i) => i !== index);
+    setTemplates(updatedTemplates);
+
+    try {
+      const response = await invoke("setTemplates", {
+        email: email.trim(),
+        templates: updatedTemplates,
+      });
+      setMessage(
+        response?.success
+          ? "List deleted successfully."
+          : "Failed to delete the list."
+      );
+    } catch {
+      setMessage("Error deleting the list.");
+    }
   };
 
   return (
-    <Box>
-      <Heading as="h1">Create a New List</Heading>
+    <>
+      <Box paddingBlockEnd="space.200">
+        <Heading as="h1">Create a New List</Heading>
+      </Box>
       {message && (
         <SectionMessage appearance="information">
           <Text>{message}</Text>
         </SectionMessage>
       )}
+      <Inline space="space.300">
+        <Box xcss={createNewListBlock}>
+          <Label>List Name</Label>
+          <Textfield
+            value={newListName}
+            onChange={(e) => setNewListName(e.target.value)}
+            placeholder="Enter list name"
+          />
 
-      <Label>List Name</Label>
-      <Textfield
-        // value={newListName}
-        onChange={(e) => setNewListName(e.target.value)}
-        placeholder="Enter list name"
-      />
+          <Label>List Item</Label>
+          <Inline alignBlock="center" alignInline="center" space="space.200">
+            <Textfield
+              value={newListItems}
+              onChange={(e) => setNewListItems(e.target.value)}
+              placeholder="Enter list item"
+            />
+            <Button appearance="primary" onClick={addListItems}>
+              +
+            </Button>
+          </Inline>
 
-      <Label>List Item</Label>
-      <Inline alignBlock="center" alignInline="center" space="space.200">
-        <Textfield
-          // value={newListItems}
-          onChange={(e) => setNewListItems(e.target.value)}
-          placeholder="Enter list item"
-        />
-        <Button onClick={addListItems}>Add Item</Button>
-      </Inline>
+          {newList.items.length > 0 && (
+            <>
+              <List>
+                <Heading as="h4">List Name: {newListName}</Heading>
+                <Heading as="h4">List Items</Heading>
 
-      {newList.items.length > 0 ? (
-        <List>
-          <Heading as="h3">List Items</Heading>
-          {newList.items.map((item, index) => (
-            <ListItem key={index}>{item.text}</ListItem>
-          ))}
-        </List>
-      ) : (
-        <Text>No items added yet.</Text>
-      )}
-
-      <Inline alignBlock="center" space="space.200">
-        <Button appearance="primary" onClick={handleSave}>
-          Save
-        </Button>
-        <Button appearance="secondary" onClick={handleClear}>
-          Clear
-        </Button>
-      </Inline>
-
-      <Box paddingBlock="space.200">
-        <Heading as="h2">My Lists</Heading>
-        {templates.map((template, index) => (
-          <Box key={index} padding="space.100">
-            <Inline space="space.400">
-              <Text>{index + 1}</Text>
-              <Heading as="h3">{template.name}</Heading>
-              <Popup
-                isOpen={openPopupIndex === index} // Only open the popup for the selected index
-                onClose={() => setOpenPopupIndex(null)} // Close the popup when clicking outside
-                placement="top-start"
-                content={() => (
-                  <Box>
-                    <Heading as="h5">Task List</Heading>
-                    <List type="ordered">
-                      {template.items.map((item, i) => (
-                        <ListItem key={i}>{item.text}</ListItem>
-                      ))}
-                    </List>
-                  </Box>
-                )}
-                trigger={() => (
-                  <Button
-                    appearance="primary"
-                    onClick={() => togglePopup(index)} // Toggle the popup for this index
-                  >
-                    {openPopupIndex === index ? "Close" : "Open"} Tasks
-                  </Button>
-                )}
-              />
+                {newList.items.map((item, index) => (
+                  <ListItem key={index}>{item.text}</ListItem>
+                ))}
+              </List>
+            </>
+          )}
+          <Box paddingBlockStart="space.200">
+            <Inline alignBlock="center" alignInline="end" space="space.100">
+              <Button appearance="primary" onClick={handleSave}>
+                Save
+              </Button>
+              <Button iconAfter="eye" onClick={handleClear}>
+                Clear
+              </Button>
             </Inline>
           </Box>
-        ))}
-      </Box>
-    </Box>
+        </Box>
+
+        <Box paddingBlock="space.200" xcss={ListBlockStyles}>
+          <Heading as="h1">My Lists</Heading>
+          {loading ? (
+            <Box padding="space.200">
+              <Spinner size="large" />
+            </Box>
+          ) : templates.length > 0 ? (
+            templates.map((template, index) => (
+              <Box key={index} padding="space.100">
+                <Inline space="space.400" spread="space-between">
+                  <Inline
+                    alignBlock="center"
+                    alignInline="center"
+                    space="space.200"
+                  >
+                    <Heading as="h4">{index + 1}</Heading>
+                    <Heading as="h4">
+                      {template.name.length > 90
+                        ? template.name.slice(0, 80) + "..."
+                        : template.name}
+                    </Heading>
+                  </Inline>
+                  <Inline space="space.100">
+                    {" "}
+                    <Button
+                      appearance="warning"
+                      onClick={() => setActiveModal(template.name)}
+                    >
+                      View
+                    </Button>
+                    <Button
+                      appearance="danger"
+                      onClick={() => handleDeleteList(index)}
+                    >
+                      Delete
+                    </Button>
+                  </Inline>
+                  <ModalTransition>
+                    {activeModal === template.name && (
+                      <Modal onClose={() => setActiveModal(null)}>
+                        <ModalHeader>
+                          <ModalTitle>{template.name}</ModalTitle>
+                        </ModalHeader>
+                        <ModalBody>
+                          <List>
+                            {template.items.map((item, index) => (
+                              <ListItem key={index}>{item.text}</ListItem>
+                            ))}
+                          </List>
+                        </ModalBody>
+                        <ModalFooter>
+                          <Button onClick={() => setActiveModal(null)}>
+                            Close
+                          </Button>
+                        </ModalFooter>
+                      </Modal>
+                    )}
+                  </ModalTransition>
+                </Inline>
+              </Box>
+            ))
+          ) : (
+            <Box>
+              <Heading as="h3">No templates found</Heading>
+            </Box>
+          )}
+        </Box>
+      </Inline>
+    </>
   );
 };
 
--- src/frontend/index.jsx
+++ src/frontend/index.jsx
@@ -1,27 +1,36 @@
 import React, { useEffect, useState } from "react";
 import ForgeReconciler, {
   Inline,
-  Textfield,
+  TextArea,
   Button,
   Checkbox,
-  LoadingButton,
-  Strike,
-  Popup,
   SectionMessage,
+  ModalHeader,
+  ModalTitle,
+  ModalBody,
+  ModalFooter,
+  ModalTransition,
   Text,
   Select,
+  Lozenge,
   List,
   ListItem,
-  Fragment,
   Box,
+  ProgressBar,
   xcss,
-  Tooltip,
   Heading,
+  Strike,
+  Modal,
 } from "@forge/react";
 import { view, invoke } from "@forge/bridge";
+import { Label } from '@atlaskit/form';
+import Textfield from '@atlaskit/textfield';
 
 const Checklist = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isViewModalOpen, setIsViewModalOpen] = useState(false);
+  const [selectedTemplate, setSelectedTemplate] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [templates, setTemplates] = useState([]);
   const [issueKey, setIssueKey] = useState("");
   const [inputStatus, setInputStatus] = useState("To Do");
   const [textInput, setTextInput] = useState("");
@@ -29,119 +38,97 @@ const Checklist = () => {
   const [isAddingTask, setIsAddingTask] = useState(false);
   const [message, setMessage] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [modalMessage, setModalMessage] = useState(null);
 
   const contentStyles = xcss({
     padding: "space.200",
+    backgroundColor: "#f4f5f7",
+    borderRadius: "8px",
+    boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
   });
 
   useEffect(() => {
-    const initialize = async () => {
-      const context = await view.getContext();
-      const currentIssueKey = context.extension.issue.id;
-      setIssueKey(currentIssueKey);
+    const fetchData = async () => {
+      try {
+        const context = await view.getContext();
+        const currentIssueKey = context.extension.issue.id;
+        if (!currentIssueKey) throw new Error("Issue key not found.");
+        setIssueKey(currentIssueKey);
 
-      if (currentIssueKey) {
-        try {
-          const storedTasks = await invoke("getTasks", {
-            issueKey: currentIssueKey,
-          });
-          setTasks(storedTasks || []);
-        } catch {
-          setMessage({ type: "error", text: "Failed to fetch tasks" });
-        } finally {
-          setIsLoading(false);
-        }
+        const [storedTasks, userData] = await Promise.all([
+          invoke("getTasks", { issueKey: currentIssueKey }),
+          invoke("getMyself"),
+        ]);
+
+        setTasks(storedTasks || []);
+        if (!userData?.emailAddress) throw new Error("User email not found.");
+
+        const allTemplates = await invoke("getTemplates", {
+          email: userData.emailAddress,
+        });
+
+        setTemplates(Array.isArray(allTemplates) ? allTemplates : []);
+      } catch (error) {
+        setMessage({ type: "error", text: error.message });
+      } finally {
+        setIsLoading(false);
       }
     };
-    initialize();
+
+    fetchData();
   }, []);
 
   const handleAddTask = async (template) => {
-    if (template && template.stages) {
-      setIsAddingTask(true);
+    if (!template?.items?.length) {
+      setMessage({ type: "warning", text: "No tasks in this template." });
+      return;
+    }
 
-      const newTasks = template.stages.map((stage) => ({
-        text: `${template.name} - ${stage}`, // Corrected template literal
+    setIsAddingTask(true);
+
+    try {
+      const newTasks = template.items.map((item, i) => ({
+        text: item.text || `Task ${i + 1}`,
         status: inputStatus,
         checked: false,
       }));
-
       const updatedTasks = [...newTasks, ...tasks];
       setTasks(updatedTasks);
+      await invoke("setTasks", { issueKey, tasks: updatedTasks });
+      setMessage({ type: "success", text: `Tasks added.` });
+      setModalMessage({ type: "success", text: `Tasks added.` });
+    } catch (error) {
+      setMessage({ type: "error", text: "Failed to add tasks." });
+    } finally {
+      setIsAddingTask(false);
+      setInputStatus("To Do");
       setTextInput("");
-
-      try {
-        await invoke("setTasks", { issueKey, tasks: updatedTasks });
-        setMessage({
-          type: "success",
-          text: `${template.name} tasks added successfully`, // Fixed closing string
-        });
-      } catch {
-        setMessage({ type: "error", text: "Failed to add tasks" });
-      } finally {
-        setIsAddingTask(false);
-        setInputStatus("To Do");
-      }
     }
   };
-
-  const templates = [
-    {
-      name: "Dev",
-      stages: [
-        "Planning",
-        "Development",
-        "Testing",
-        "Review",
-        "Deployment",
-        "Maintenance",
-      ],
-    },
-    {
-      name: "UAT",
-      stages: ["Planning", "Testing", "Sign-off"],
-    },
-    {
-      name: "Software Development",
-      stages: [
-        "Requirements",
-        "Design",
-        "Implementation",
-        "Testing",
-        "Deployment",
-        "Maintenance",
-      ],
-    },
-    {
-      name: "QA",
-      stages: ["Test Planning", "Test Execution", "Bug Fixing", "Reporting"],
-    },
-    {
-      name: "Prod",
-      stages: ["Preparation", "Deployment", "Monitoring", "Post-Deployment"],
-    },
-    {
-      name: "Staging",
-      stages: ["Setup", "Pre-Testing", "Testing", "Verification"],
-    },
-  ];
 
   const handleStatusChange = async (index, status) => {
     const updatedTasks = tasks.map((task, i) =>
       i === index ? { ...task, status, checked: status === "Done" } : task
     );
-
     setTasks(updatedTasks);
 
     try {
       await invoke("setTasks", { issueKey, tasks: updatedTasks });
-    } catch {
-      setMessage({ type: "error", text: "Failed to update task status" });
+    } catch (error) {
+      setMessage({ type: "error", text: "Failed to update status." });
     }
   };
 
+  const handleViewTemplate = (template) => {
+    setSelectedTemplate(template);
+    setIsViewModalOpen(true);
+  };
+  const completedTasks = tasks.filter((task) => task.status === "Done").length;
+  const totalTasks = tasks.filter((task) => task.status !== "Done").length;
+  const progressValue = totalTasks > 0 ? completedTasks / totalTasks : 0;
+
   return (
-    <>
+    <Box>
       {message && (
         <SectionMessage appearance={message.type}>
           <Text>{message.text}</Text>
@@ -150,94 +137,158 @@ const Checklist = () => {
 
       {isLoading ? (
         <SectionMessage appearance="information">
-          <Text>Loading tasks, please wait...</Text>
+          <Text>Loading tasks...</Text>
         </SectionMessage>
       ) : (
         <>
-          <Popup
-            isOpen={isOpen}
-            onClose={() => setIsOpen(false)}
-            placement="top-start"
-            content={() => (
-              <Box xcss={contentStyles}>
-                <Heading as="h5">Task List</Heading>
-                <List type="ordered">
-                  {templates.map((template) => (
-                    <ListItem key={template.name}>
-                      <Inline space="space.400" spread="space-between">
-                        <Text>{template.name}</Text>
-                        <Inline space="space.200">
-                          <Button
-                            appearance="primary"
-                            onClick={() => handleAddTask(template)}
-                          >
-                            Add
-                          </Button>
-                          <Button
-                            appearance="warning"
-                            onClick={() => {
-                              console.log(`Viewing tasks for ${template.name}`);
-                            }}
-                          >
-                            View
-                          </Button>
-                        </Inline>
-                      </Inline>
-                    </ListItem>
-                  ))}
-                </List>
-              </Box>
-            )}
-            trigger={() => (
-              <Button
-                appearance="primary"
-                isSelected={isOpen}
-                onClick={() => setIsOpen(!isOpen)}
-              >
-                {isOpen ? "Close" : "Open"} popup
-              </Button>
-            )}
-          />
+          <Inline spread="space-between" alignBlock="center">
+            <Box>
 
-          <Inline space="space.050" alignBlock="center">
-            <Select
-              inputId="task-status-select"
-              appearance="default"
-              onChange={(e) => setInputStatus(e.value)}
-              options={[
-                { label: "To do", value: "To do" },
-                { label: "In Progress", value: "In Progress" },
-                { label: "Done", value: "Done" },
-                { label: "Skipped", value: "Skipped" },
-              ]}
-            />
-            <Textfield
-              isRequired
-              id="taskInput"
-              value={textInput}
-              onChange={(e) => setTextInput(e.target.value)}
-              placeholder="Enter a task"
-            />
-            {isAddingTask ? (
-              <LoadingButton appearance="primary" isLoading>
-                Loading button
-              </LoadingButton>
-            ) : (
+              <Text>
+                Completed {completedTasks} of {totalTasks} tasks
+              </Text>
+              <ProgressBar
+                ariaLabel={`Done: ${completedTasks} of ${totalTasks} tasks`}
+                value={progressValue}
+                appearance={
+                  completedTasks === totalTasks ? "success" : "default"
+                }
+              />
+            </Box>
+            <Button appearance="primary" onClick={() => setIsModalOpen(true)}>
+              Open Templates
+            </Button>
+          </Inline>
+          <Label htmlFor="basic-textfield">Field label</Label>
+			<Textfield name="basic" id="basic-textfield" />
+
+          {isModalOpen && (
+            <ModalTransition>
+              <Modal onClose={() => setIsModalOpen(false)} xcss={contentStyles}>
+                <ModalHeader>
+                  <ModalTitle>
+                    <Heading as="h1">Templates</Heading>
+                  </ModalTitle>
+                  {modalMessage && (
+                    <SectionMessage appearance={modalMessage.type}>
+                      <Text>{modalMessage.text}</Text>
+                    </SectionMessage>
+                  )}
+                </ModalHeader>
+                <ModalBody>
+                  <List type="unordered">
+                    {templates.map((template) => (
+                      <ListItem key={template.name}>
+                        <Inline space="space.100" spread="space-between">
+                          <Text>
+                            {template.name.length > 60
+                              ? template.name.slice(0, 50) + "..."
+                              : template.name}
+                          </Text>
+                          <Inline space="space.100">
+                            <Button
+                              appearance="warning"
+                              onClick={() => handleViewTemplate(template)}
+                            >
+                              View
+                            </Button>
+                            <Button
+                              appearance="primary"
+                              onClick={() => handleAddTask(template)}
+                            >
+                              Add
+                            </Button>
+                          </Inline>
+                        </Inline>
+                      </ListItem>
+                    ))}
+                  </List>
+                </ModalBody>
+                <ModalFooter>
+                  <Button onClick={() => setIsModalOpen(false)}>Close</Button>
+                </ModalFooter>
+              </Modal>
+            </ModalTransition>
+          )}
+
+          {isViewModalOpen && selectedTemplate && (
+            <ModalTransition>
+              <Modal
+                onClose={() => setIsViewModalOpen(false)}
+                xcss={contentStyles}
+              >
+                <ModalHeader>
+                  <ModalTitle>
+                    <Heading as="h1">{selectedTemplate.name}</Heading>
+                  </ModalTitle>
+                </ModalHeader>
+                <ModalBody>
+                  <List type="unordered">
+                    {selectedTemplate.items.map((item, index) => (
+                      <ListItem key={index}>{item.text}</ListItem>
+                    ))}
+                  </List>
+                </ModalBody>
+                <ModalFooter>
+                  <Button onClick={() => setIsViewModalOpen(false)}>
+                    Close
+                  </Button>
+                </ModalFooter>
+              </Modal>
+            </ModalTransition>
+          )}
+
+          <Box
+            xcss={{
+              paddingBlock: "space.100",
+            }}
+          >
+            <Inline alignBlock="center" space="space.050">
+              <Select
+                onChange={(e) => setInputStatus(e.value)}
+                options={[
+                  { label: "To do", value: "To Do" },
+                  { label: "In Progress", value: "In Progress" },
+                  { label: "Done", value: "Done" },
+                  { label: "Skipped", value: "Skipped" },
+                ]}
+              />
+
+              <TextArea
+                value={textInput}
+                onChange={(e) => setTextInput(e.target.value)}
+                placeholder="Enter a task"
+              />
               <Button
                 onClick={() =>
-                  handleAddTask({ stages: [textInput], name: "Custom" })
+                  handleAddTask({
+                    items: [{ text: textInput }],
+                    name: "Custom",
+                  })
                 }
                 appearance="primary"
-                isLoading={isAddingTask}
+                isDisabled={isAddingTask || !textInput}
               >
-                Add Task
+                {isAddingTask ? "Adding..." : "Add"}
               </Button>
-            )}
-          </Inline>
+            </Inline>
+          </Box>
 
-          {tasks.length > 0 &&
-            tasks.map((task, index) => (
-              <Box paddingBlockStart="space.200" key={task.text + index}>
+          {tasks.map((task, index) => {
+            const boxStyles = xcss({
+              backgroundColor:
+                task.status === "Done"
+                  ? "color.background.success"
+                  : task.status === "In Progress"
+                  ? "color.background.warning"
+                  : task.status === "Skipped"
+                  ? "color.background.danger.hovered"
+                  : "color.background.accent.blue.subtlest",
+              padding: "space.100",
+            });
+
+            return (
+              <Box key={task.text + index} xcss={boxStyles}>
                 <Inline alignBlock="center" space="space.100">
                   <Checkbox
                     onChange={(e) =>
@@ -249,13 +300,13 @@ const Checklist = () => {
                     isChecked={task.checked}
                   />
                   <Select
+                    xcss={boxStyles}
+                    defaultValue={task.status}
                     value={{ label: task.status, value: task.status }}
-                    placeholder={task.status}
                     inputId={`task-status-select-${index}`}
-                    appearance="default"
                     onChange={(e) => handleStatusChange(index, e.value)}
                     options={[
-                      { label: "To do", value: "To do" },
+                      { label: "To do", value: "To Do" },
                       { label: "In Progress", value: "In Progress" },
                       { label: "Done", value: "Done" },
                       { label: "Skipped", value: "Skipped" },
@@ -263,17 +314,24 @@ const Checklist = () => {
                   />
                   <Heading as="h5">
                     {task.status === "Done" ? (
-                      <Strike>{task.text}</Strike>
+                      <Strike>
+                        {task?.text?.length > 30
+                          ? task.text.slice(0, 30) + "..."
+                          : task.text}
+                      </Strike>
+                    ) : task?.text?.length > 30 ? (
+                      task.text.slice(0, 30) + "..."
                     ) : (
-                      <Text>{task.text}</Text>
+                      task.text
                     )}
                   </Heading>
                 </Inline>
               </Box>
-            ))}
+            );
+          })}
         </>
       )}
-    </>
+    </Box>
   );
 };
 
